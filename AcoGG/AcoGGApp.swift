840a0d637a5b3d1cf41c9d90cf8d457ac6f3862b
Aco <ozenahmetcemil@gmail.com>
2025-11-19 15:58:17 +0300

    Initial Commit

---
+++ AcoGG/AcoGGApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcoGGApp.swift
+//  AcoGG
+//
+//  Created by Ahmet Ozen on 19.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AcoGGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
